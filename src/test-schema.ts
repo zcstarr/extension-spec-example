7c92869daca4f49b056512fe3730e3f0e3ea8e34
Zane Starr <zcstarr@gmail.com>
2025-01-22 09:54:23 -0800

    chore: lint

--- src/test-schema.ts
+++ src/test-schema.ts
@@ -5,16 +5,15 @@ import errorGroupSupportedDocument from "./error-groups-schema.json"
 async function testSchema(){
   const dereffed = await dereferenceDocument(errorGroupSupportedDocument as OpenrpcDocument);
   // Run validation
-  //const result = validateOpenRPCDocument(goodExtensionDoc as OpenrpcDocument);
   console.log(JSON.stringify(dereffed, null, 2))
   const result = validateOpenRPCDocument(dereffed as OpenrpcDocument);
 
-// Pretty print results
-if (result === true) {
-  console.log("✅ Schema is valid!");
-} else {
-  console.log("❌ Schema validation failed:");
-  console.log(result.message);
-} 
+  // Pretty print results
+  if (result === true) {
+    console.log("✅ Schema is valid!");
+  } else {
+    console.log("❌ Schema validation failed:");
+    console.log(result.message);
+  } 
 }
 testSchema()
